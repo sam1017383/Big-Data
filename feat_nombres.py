46d365cc976c816a27108e6f143c7642def859a3
sam1017383 <1017383@gmail.com>
2015-08-13 11:15:58 -0500

    tweets

---
+++ feat_nombres.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+
+
+
+names = [
+"ADELFRIED",
+"BERNADETTE",
+"CARLEIGH",
+"DAGOBERT",
+"ELISABETH",
+"FLOY",
+"GILBERT",
+"HASTINGS",
+"ILSE",
+"JOHANN",
+"KELLEN",
+"LORING",
+"MAUD",
+"NEVIN",
+"ODELL",
+"PENROD",
+"RITTER",
+"STEIN",
+"THEOBOLD",
+"ULBRECHT",
+"VILHELM",
+"WARNER",
+"ZELIG"
+]
+
+nombres = [
+"FABIOLA",
+"FEDERICO",
+"ADOLFO",
+"ALEJANDRO",
+"ARACELI",
+"AURORA",
+"CAMILA",
+"CONSUELO",
+"GUILLERMO",
+"GLORIA",
+"MATEO",
+"MAXIMILIANO",
+"WILFREDO",
+"DANIEL",
+"DOLORES",
+"JACINTO",
+"JAIME",
+"JULIANA",
+"REBECA",
+"RODRIGO",
+"ELENA",
+"VALERIA",
+"ERNESTO",
+
+]
+
+
+def obtener_caracteristicas(lista_nombres):
+	X = []
+	Y = []
+	for cada_nombre in lista_nombres:
+		print "cada nombre: ", cada_nombre
+		#longitud = 
+		#vocales = 
+		#print "VOCALES: ", vocales
+		#voc_con = float(vocales) / (len(cada_nombre) - vocales)
+
+		#X.append(longitud)
+		#Y.append(voc_con)
+	return X, Y
+
+
+X_e, Y_e = obtener_caracteristicas(nombres)
+X_a, Y_a = obtener_caracteristicas(names)
+
+print "coeficiente de espanol: ", Y_e
+print "coeficiente de aleman: ", Y_a
+
+print 2 in [1,2,3,4,5]
+print 6 in [1,2,3,4,5]
+
+plt.plot(X_e, Y_e, 'bo')
+plt.plot(X_a, Y_a, 'ro')
+plt.axis([0, 15, -3, 3])
+plt.show()
+
+
